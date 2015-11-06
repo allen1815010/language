fd6f535321895161589c7c67b8dace5ad197d56f
m516 <m516@m516>
2015-11-07 00:18:53 +0800

    fu

--- language/wiki/views.py
+++ language/wiki/views.py
@@ -1,12 +1,24 @@
 from django.shortcuts import render
-
+from wiki.models import Category, Page
 
 
 def wiki(request):
-    context = {}
+    categories = Category.objects.order_by('-likes')
+    context = {'categories':categories}
     return render(request, 'wiki/wiki.html', context)
 
 
 def about(request):
     context = {}
     return render(request, 'wiki/about.html', context)
+
+def category(request, categoryNameSlug):
+    context = {}
+    try:
+        category = Category.objects.get(slug=categoryNameSlug)
+        context['category'] = category
+        pages = Page.objects.filter(category=category)
+        context['pages'] = pages
+    except Category.DoesNotExist:
+        pass
+    return render(request, 'wiki/category.html', context)
